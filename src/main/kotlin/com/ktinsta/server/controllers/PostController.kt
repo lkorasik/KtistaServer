95dc264ab70d8fdc047802e68c701addc7948084
Lev <lkorasik83@gmail.com>
2021-12-11 23:03:39 +0500

    Вынес класс

--- src/main/kotlin/com/ktinsta/server/controllers/PostController.kt
+++ src/main/kotlin/com/ktinsta/server/controllers/PostController.kt
@@ -1,6 +1,7 @@
 package com.ktinsta.server.controllers
 
 import com.ktinsta.server.constants.ResponseConstants
+import com.ktinsta.server.helpers.objects.PostVO
 import com.ktinsta.server.model.Image
 import com.ktinsta.server.model.Post
 import com.ktinsta.server.service.PostService
@@ -12,13 +13,12 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import javax.servlet.http.HttpServletResponse
 import javax.validation.Valid
-import kotlin.coroutines.coroutineContext
 
 @RestController
 @RequestMapping("/api/post/")
 class PostController(val postService: PostService, val userService: UserService){
     @PostMapping("/create")
-    fun createPost(@Valid @RequestBody postDetails: PostNet, response: HttpServletResponse): ResponseEntity<Any> {
+    fun createPost(@Valid @RequestBody postDetails: PostVO, response: HttpServletResponse): ResponseEntity<Any> {
         val author = userService.retrieveUserData(postDetails.userId)
 
         val post = Post(author = author, text = postDetails.text)
@@ -29,5 +29,3 @@ class PostController(val postService: PostService, val userService: UserService)
         return ResponseEntity.ok(ResponseConstants.SUCCESS.value)
     }
 }
-
-data class PostNet(var userId: Long, var text: String, var data: ByteArray)
