935557ff01550669afa3f4863c26045bd8a7edff
Lev <lkorasik83@gmail.com>
2021-12-11 19:41:46 +0500

    Create empty api method

--- src/main/kotlin/com/ktinsta/server/controllers/UserController.kt
+++ src/main/kotlin/com/ktinsta/server/controllers/UserController.kt
@@ -4,6 +4,7 @@ import com.ktinsta.server.components.UserAssembler
 import com.ktinsta.server.constants.ResponseConstants
 import com.ktinsta.server.helpers.objects.LoginVO
 import com.ktinsta.server.helpers.objects.UserVO
+import com.ktinsta.server.model.Post
 import com.ktinsta.server.model.User
 import com.ktinsta.server.repository.UserRepository
 import com.ktinsta.server.service.UserServiceImpl
@@ -16,14 +17,24 @@ import java.util.*
 import javax.servlet.http.Cookie
 import javax.servlet.http.HttpServletResponse
 import javax.validation.Valid
+import kotlin.collections.HashMap
+
+@RestController
+@RequestMapping("/api/post/")
+class PostController{
+    @PostMapping("/create")
+    fun createPost(@Valid @RequestBody post: PostNet, response: HttpServletResponse): ResponseEntity<Any>{
+        println("Create post")
+
+        return ResponseEntity.ok(ResponseConstants.SUCCESS.value)
+    }
+}
+
+data class PostNet(var userId: Long, var text: String, var data: ByteArray)
 
 @RestController
 @RequestMapping("/api/user")
-class UserController(val userRepository: UserRepository,
-                     val userService: UserServiceImpl,
-                     val userAssembler: UserAssembler) {
-
-
+class UserController(val userRepository: UserRepository, val userService: UserServiceImpl, val userAssembler: UserAssembler) {
     @PostMapping("/registration")
     fun createNewUser(@Valid @RequestBody userDetails: User, response: HttpServletResponse): ResponseEntity<Any> {
         val user = userService.attemptRegistration(userDetails)
