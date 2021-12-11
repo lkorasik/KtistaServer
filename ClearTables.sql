8b64c2565c3f30907a84d88e795966d11ef7213d
Lev <lkorasik83@gmail.com>
2021-12-11 20:35:57 +0500

    Update ClearTables.sql

--- ClearTables.sql
+++ ClearTables.sql
@@ -1,7 +1,7 @@
-DROP TABLE public."followers_user";
-DROP TABLE public."image";
-DROP TABLE public."post_dislike";
-DROP TABLE public."post_like";
-DROP TABLE public."post";
-DROP TABLE public."followers";
-DROP TABLE public."user";
+DROP TABLE if exists public."followers_user";
+DROP TABLE if exists public."image";
+DROP TABLE if exists public."post_dislike";
+DROP TABLE if exists public."post_like";
+DROP TABLE if exists public."post";
+DROP TABLE if exists public."followers";
+DROP TABLE if exists public."user";
