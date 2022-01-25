c98fd850dee6e82a345433ebbaeb398c3bfd5ded
lev <lkorasik83@gmail.com>
2022-01-25 12:50:54 +0300

    Добавление id пользователя в jwt

--- src/main/kotlin/com/ktinsta/server/security/service/TokenAuthenticationService.kt
+++ src/main/kotlin/com/ktinsta/server/security/service/TokenAuthenticationService.kt
@@ -1,5 +1,7 @@
 package com.ktinsta.server.security.service
 
+import com.ktinsta.server.service.UserService
+import io.jsonwebtoken.Jwt
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
 import org.springframework.beans.factory.annotation.Value
@@ -11,15 +13,15 @@ import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 internal object TokenAuthenticationService {
-
     private val TOKEN_EXPIRY: Long = 600000000000
     private val SECRET = "secret"
     private val TOKEN_PREFIX = "Bearer"
     private val AUTHORIZATION_HEADER_KEY = "Authorization"
 
-    fun addAuthentication(response: HttpServletResponse, username: String) {
+    fun addAuthentication(response: HttpServletResponse, username: String, id: Long) {
         val jwt = Jwts.builder()
             .setSubject(username)
+            .claim("ID", id)
             .setExpiration(Date(System.currentTimeMillis() + TOKEN_EXPIRY))
             .signWith(SignatureAlgorithm.HS512, SECRET)
             .compact()
@@ -33,6 +35,7 @@ internal object TokenAuthenticationService {
             val user = Jwts.parser().setSigningKey(SECRET)
                 .parseClaimsJws(token.replace(TOKEN_PREFIX, ""))
                 .body.subject
+            //val userId = Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token.replace(TOKEN_PREFIX, "")).body["ID"]
 
             if (user != null)
                 return UsernamePasswordAuthenticationToken(user, null, emptyList<GrantedAuthority>())
