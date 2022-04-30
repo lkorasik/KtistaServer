45296c5886587e4e07db438f532c95953c898b0b
lev <lkorasik83@gmail.com>
2022-04-30 16:12:38 +0500

    Make output in action more pretty

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 	kotlin("jvm") version "1.5.31"
 	kotlin("plugin.spring") version "1.5.31"
 	kotlin("plugin.jpa") version "1.5.31"
+	id("com.adarshr.test-logger") version "3.2.0"
 }
 
 group = "com.ktinsta"
@@ -34,6 +35,53 @@ dependencies {
 	testImplementation("org.assertj:assertj-core:3.22.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
+}
+
+testlogger {
+	// pick a theme - mocha, standard, plain, mocha-parallel, standard-parallel or plain-parallel
+	theme = com.adarshr.gradle.testlogger.theme.ThemeType.STANDARD
+
+	// set to false to disable detailed failure logs
+	showExceptions = true
+
+	// set to false to hide stack traces
+	showStackTraces = true
+
+	// set to true to remove any filtering applied to stack traces
+	showFullStackTraces = false
+
+	// set to false to hide exception causes
+	showCauses = true
+
+	// set threshold in milliseconds to highlight slow tests
+	slowThreshold = 2000
+
+	// displays a breakdown of passes, failures and skips along with total duration
+	showSummary = true
+
+	// set to true to see simple class names
+	showSimpleNames = false
+
+	// set to false to hide passed tests
+	showPassed = true
+
+	// set to false to hide skipped tests
+	showSkipped = true
+
+	// set to false to hide failed tests
+	showFailed = true
+
+	// enable to see standard out and error streams inline with the test results
+	showStandardStreams = false
+
+	// set to false to hide passed standard out and error streams
+	showPassedStandardStreams = false
+
+	// set to false to hide skipped standard out and error streams
+	showSkippedStandardStreams = false
+
+	// set to false to hide failed standard out and error streams
+	showFailedStandardStreams = false
 }
 
 tasks.withType<KotlinCompile> {
